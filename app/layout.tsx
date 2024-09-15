4cc47447670a3173434518e390860848288cd3e4
Dg <dnechtan@gmail.com>
2024-09-15 03:06:29 -0300

    Integrate usePollinationsChat and PollinationsImage components
    - Added usePollinationsChat hook for managing chat interactions.
    - Incorporated PollinationsImage component for displaying generated images.
    - Updated components and logic to utilize new hooks and components effectively.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -14,10 +15,12 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Pollinations - React Example",
-  description: "Karma.yt",
-};
-
+  title: "Chat with Pollinations",
+  description: "Generate Art - Pollinations - Karma.yt",
+  keywords: ["Karma", "Chat", "Karma.yt", "Pollinations.ai", "AI", "image generation"].join(', '), openGraph: {
+    images: '/opengraph-image.png'
+  }
+}
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -25,6 +28,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-4QJ7GS0M0S"
+        />
+        <Script id="google-analytics">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-4QJ7GS0M0S');
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
